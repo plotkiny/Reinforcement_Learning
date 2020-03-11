bddaa881cc9e5608590ad2e1cc3f1e4c52a0d70d
Yuri <plotkiny@gmail.com>
2020-03-11 11:28:04 -0700

    replay buffer inheritence update

--- utils/replay_buffer.py
+++ utils/replay_buffer.py
@@ -13,7 +13,68 @@ from collections import namedtuple, deque
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class PriorityReplayBuffer:
+
+class ReplayBuffer(object):
+    '''Fixed-size buffer to store experience tuples.'''
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        '''Initialize a ReplayBuffer object.
+        :param action_size: int. dimension of each action
+        :param buffer_size: int: maximum size of buffer
+        :param batch_size (int): size of each training batch
+        :param seed (int): random seed
+        '''
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience",
+                                     field_names=["state", "action", "reward",
+                                                  "next_state", "done",
+                                                  "others_states",
+                                                  "others_actions",
+                                                  "others_next_states"])
+        self.seed = random.seed(seed)
+
+    def add(self, state, action, reward, next_state, done, others_states,
+            others_actions, others_next_states):
+        '''Add a new experience to memory.'''
+        e = self.experience(state, action, reward, next_state, done,
+                            others_states, others_actions, others_next_states)
+        self.memory.append(e)
+
+    def sample(self):
+        '''Randomly sample a batch of experiences from memory.'''
+        experiences = random.sample(self.memory, k=self.batch_size)
+
+        states = torch.from_numpy(np.vstack([e.state for e in experiences
+                                  if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences
+                                   if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences
+                                   if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state
+                                                  for e in experiences
+                                                  if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences
+                                            if e is not None]).astype(np.uint8)).float().to(device)
+
+        others_states = torch.from_numpy(np.vstack([e.others_states for e in experiences
+                                  if e is not None])).float().to(device)
+        others_actions = torch.from_numpy(np.vstack([e.others_actions for e in experiences
+                                   if e is not None])).float().to(device)
+        others_next_states = torch.from_numpy(np.vstack([e.others_next_states
+                                                      for e in experiences
+                                                      if e is not None])).float().to(device)
+
+        return (states, actions, rewards, next_states, dones, others_states,
+                others_actions, others_next_states)
+
+    def __len__(self):
+        '''Return the current size of internal memory.'''
+        return len(self.memory)
+
+
+class PriorityReplayBuffer(ReplayBuffer):
     """Fixed-size buffer to store experience tuples."""
 
     def __init__(self, action_size, buffer_size, batch_size, seed, p_alpha=0.75, p_beta=0.5, beta_frames=10000):
@@ -36,7 +97,7 @@ class PriorityReplayBuffer:
         self.beta_frames = beta_frames
         self.buffer_size = buffer_size
         self.current_frame = 1
-        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done", ])
         self.memory = deque(maxlen=buffer_size)
         self.p_alpha = p_alpha
         self.p_beta = p_beta
